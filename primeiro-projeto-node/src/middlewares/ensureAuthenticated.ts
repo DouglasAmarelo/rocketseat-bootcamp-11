5606e76041703918181c86b8082970f1f7003b85
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2020-05-01 16:59:34 -0300

    class to error tratament

--- primeiro-projeto-node/src/middlewares/ensureAuthenticated.ts
+++ primeiro-projeto-node/src/middlewares/ensureAuthenticated.ts
@@ -3,6 +3,8 @@ import { verify } from 'jsonwebtoken';
 
 import authConfig from '../config/auth';
 
+import AppError from '../errors/AppError';
+
 interface TokenPayload {
   iat: number;
   exp: number;
@@ -17,7 +19,7 @@ const ensureAuthenticated = (
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error('JWT Token is missing');
+    throw new AppError('JWT Token is missing', 401);
   }
 
   const [, token] = authHeader.split(' ');
@@ -35,7 +37,7 @@ const ensureAuthenticated = (
 
     return next();
   } catch (error) {
-    throw new Error('Invalid JWT token');
+    throw new AppError('Invalid JWT token', 401);
   }
 };
 
